d4e04e61d18f982be828789273f990acf2377ab9
Pedro <pedrogoku80@gmail.com>
2024-11-04 17:52:56 -0300

    algoritmo principal

---
+++ algoritmoCicloGraph.py
@@ -0,0 +1,37 @@
+adj_list = {
+    "A": ["C", "B"],
+    "B": ["D"],
+    "C": [],
+    "D": ["A", "E"],
+    "E": []
+}
+
+color = {}
+parent = {}
+
+for u in adj_list.keys():
+    color[u] = "W"
+    parent[u] = None
+
+def dfs(u, color, parent):
+    color[u] = "G"
+
+    for v in adj_list[u]:
+        if color[v] == "W":
+            parent[v] = u
+            cycle = dfs(v, color, parent)
+            if cycle == True:
+                return True
+        elif color[v] == "G" and parent[u] != v:
+            print("Cycle found", u, v)
+            return True
+    color[u] = "B"
+    return False
+
+is_cyclic = False
+for u in adj_list.keys():
+    if color[u] == "W":
+        is_cyclic = dfs(u, color, parent)
+        if is_cyclic == True:
+            break
+print("Is cycli ", is_cyclic)
